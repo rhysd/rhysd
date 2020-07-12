3068b693c31495824ce421ac6a9489c9d4320b55
rhysd <lin90162@yahoo.co.jp>
2020-07-12 13:44:21 +0900

    tweak 'y' in quine

--- q/aa.rb
+++ q/aa.rb
@@ -1,13 +1,13 @@
               eval                                                   ($s=
               %w(s                                                   =%(e
               val(                                                   $s=%
-w(#{  $s})*   "")) ;puts(   ["1f      wnv1q    4xrvp4w0"       ,"1fw nv1q
-4xrvp4w0","   1fwnv1q4xrvp   4w0"     ,"1h    l0q9d  iho9     sclr","1jca
-npzbod        nf30f   ","1    hu91   oefg     wcfd           xb",   "1g03
-1fmt9         cb4o    in",     "1g0 3kvf       pbxhgtu7"     ,"1     hua2
-h60u          blvr    wv",      "1jcapk           ghpfdh2    j3"     ,"1h
-l0ga          dqdu    qkvt       b","v        bm5    vcw"    ,"7n   8t7nk
-"].m          ap{|    l|((        0..         72).map{|i|     l.to_i(36)[
-i]>0          ?s.s    lice       !(0)            :32.ch        r}*"" ).rs
-                             trip})#
+w(#{  $s})*   "")) ;puts(   ["1f       wnv1    q4xrvp4w0       ","1f wnv1
+q4xrvp4w0",   "1fwnv1q4xrv   p4w0     ","1    hl0q9  deze     aku6n","1jc
+anpzbo        dnf30   f","    1hu9   1oef     gwcf           dxb"   ,"1g0
+31fmt         9cb4    oin"     ,"1g 03kv       fpbxhgtu7     ","     1hua
+2h60          ublv    rwv"      ,"1jcap           kghtdfp    6hb     ","1
+hl0g          adpk    99w2       6n","        fjd    9fk0    ","7   n8t7n
+k"].          map{    |l|(       (0..         72).map{|i|     l.to_i(36)[
+i]>0          ?s.s    lice      !(0)             :32.ch        r}*"" ).rs
+                             trip})
                              )*"")
